34994d5a49092ce4be9306e9d1a06401362f5af1
Maksim Fedorov <xmaksimxfedorovx@gmail.com>
2021-02-05 11:06:17 +0300

    Add xxxhdpi support

--- converter.sh
+++ converter.sh
@@ -9,6 +9,7 @@ if ! [ -f "$1" ]; then
 fi
 
 echo " Creating different dimensions (dips) of "$1" ..."
+mkdir -p drawable-xxxhdpi
 mkdir -p drawable-xxhdpi
 mkdir -p drawable-xhdpi
 mkdir -p drawable-hdpi
@@ -16,16 +17,18 @@ mkdir -p drawable-mdpi
 
 if [ $1 = "ic_launcher.png" ]; then
 		echo "  App icon detected"
+	convert ic_launcher.png -resize 192x192 drawable-xxxhdpi/ic_launcher.png
 	convert ic_launcher.png -resize 144x144 drawable-xxhdpi/ic_launcher.png
 	convert ic_launcher.png -resize 96x96 drawable-xhdpi/ic_launcher.png
 	convert ic_launcher.png -resize 72x72 drawable-hdpi/ic_launcher.png
 	convert ic_launcher.png -resize 48x48 drawable-mdpi/ic_launcher.png
 	rm -i ic_launcher.png
 else
+	convert $1 -resize 75% drawable-xxhdpi/$1
 	convert $1 -resize 67% drawable-xhdpi/$1
 	convert $1 -resize 50% drawable-hdpi/$1
 	convert $1 -resize 33% drawable-mdpi/$1
-	mv $1 drawable-xxhdpi/$1
+	mv $1 drawable-xxxhdpi/$1
 fi
 
 echo " Done"
